61083d40e2682b845c715f1cd41a6e4f0a0c45e1
Francisco Guerreiro <francisco.guerreiro.jr@gmail.com>
2015-03-20 17:37:36 -0300

    Feito de #12 a #23

--- backend/milfaq/app/models/perfil.rb
+++ backend/milfaq/app/models/perfil.rb
@@ -1,2 +1,4 @@
 class Perfil < ActiveRecord::Base
+	validates :descricao, presence: true,
+                      	  uniqueness: true
 end
--- backend/milfaq/app/models/problema.rb
+++ backend/milfaq/app/models/problema.rb
@@ -1,2 +1,5 @@
 class Problema < ActiveRecord::Base
+	belongs_to :usuario
+	validates :relator, presence: true
+	validates :descricao, length: { maximum: 500 }
 end
